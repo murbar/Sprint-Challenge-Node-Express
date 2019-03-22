1b2abcca947cf0a0304e9e49d8df8d4fe39b26b6
Joel Bartlett <joel@murbar.com>
2019-03-22 13:26:49 -0400

    Create react app

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Router } from '@reach/router';
+
+const App = () => {
+  return (
+    <main>
+      <Router>
+        <h1>Projects list</h1>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
